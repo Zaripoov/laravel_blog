6b85ccf1b55f4ddd08cd07a40337bbdf1fd7a967
Zaripov R <ruzal.zarip1324@gmail.com>
2021-07-29 21:45:45 +0300

    show tags

--- app/Http/Controllers/TagController.php
+++ app/Http/Controllers/TagController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Tag;
 use Illuminate\Http\Request;
 
 class TagController extends Controller
 {
-    //
+    public function show($slug){
+
+        $tag = Tag::where('slug', $slug)->firstOrFail();
+
+        $posts = $tag->posts()->with('category')->orderBy('id', 'desc')->paginate(2);
+
+        return view('tags.show',[
+            'tag' => $tag,
+            'posts' => $posts,
+        ]);
+    }
 }
